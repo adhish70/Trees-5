f3f5761836fec71aa8e1e53ce9889953ba6a66be
Adhish Shrivastava <adhish70@gmail.com>
2021-12-08 10:30:53 -0500

    Initial Commit

---
+++ Problem-104 Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,21 @@
+# 116. Populating Next Right Pointers in Each Node
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+# Logic: 
+
+# Time Complexity:
+# Space Complexity: 
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:         
+    def connect(self, root: 'Node') -> 'Node':
+        pass
